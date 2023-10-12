9d2f58a996e6179dd8b5cfd1a471df8651c66f4d
juandpadilla@uninorte.edu.co <juandpadilla@uninorte.edu.co>
2023-10-11 22:01:03 -0500

    setup database implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV.trim()}.env`,
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({...DataSourceConfig})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
