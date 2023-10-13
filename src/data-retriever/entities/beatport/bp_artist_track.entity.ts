875391d7bd8292761fb62605b5990729f43567b7
FiboDev <juanpadilla@uninorte.edu.co>
2023-10-13 19:09:07 +0000

    relations added to beatport

--- src/data-retriever/entities/beatport/bp_artist_track.entity.ts
+++ src/data-retriever/entities/beatport/bp_artist_track.entity.ts
@@ -1,12 +1,18 @@
-import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BeatportRelease } from "./bp_release.entity";
+import { BeatportTrack } from "./bp_track.entity";
 
 @Entity({name: 'bp_artist_track'})
 export class BeatportArtistTrack {
 
     @PrimaryGeneratedColumn({name: 'artist_id'})
+    @OneToOne(() => BeatportRelease, release => release.releaseId)
+    @JoinColumn()
     releaseId: number
 
     @Column({name: 'track_id'})
+    @OneToOne(() => BeatportTrack, track => track.trackId)
+    @JoinColumn()
     trackId: number
 
     @UpdateDateColumn({
