0a46f8e422e6dae672ea29473157f3f778582250
FiboDev <juanpadilla@uninorte.edu.co>
2023-10-13 17:23:29 +0000

    testing

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,4 +1,5 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SpotifyArtistEntity } from 'src/data-retriever/entities/spotify/sp_artist.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
@@ -15,7 +16,7 @@ export const DataSourceConfig: DataSourceOptions = {
     username: configService.get('POSTGRES_USER'),
     password: configService.get('POSTGRES_PASSWORD'),
     database: configService.get('POSTGRES_DB'),
-    entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+    entities: [SpotifyArtistEntity],
     migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
     synchronize: true,
     migrationsRun: true,
