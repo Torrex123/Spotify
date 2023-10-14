48576062503679a1e7f73a82dbb036ba4fa596f3
FiboDev <juanpadilla@uninorte.edu.co>
2023-10-14 02:06:12 +0000

    factories added

---
+++ db/factories/audio_features.factory.ts
@@ -0,0 +1,23 @@
+import { setSeederFactory } from 'typeorm-extension';
+import { AudioFeatures } from 'src/data-retriever/entities/audio/audio_features.entity';
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
+
+export default setSeederFactory(AudioFeatures, async () => {
+
+  const csvData = [];
+
+  const csvFilePath = "csv_files/audio_features.csv"
+
+  fs.createReadStream(csvFilePath)
+    .pipe(parse({ delimiter: ',' , columns: true}))
+    .on('data', (row) => {
+      csvData.push(row);
+      console.log(row);
+    });
+
+  const audioFeatures = new AudioFeatures();
+
+    return audioFeatures;
+});
--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,6 +1,7 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import path from 'path';
+import InitSeeder from 'db/seeds/init.seeder';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 ConfigModule.forRoot({
@@ -8,6 +9,22 @@ ConfigModule.forRoot({
 });
 
 const configService = new ConfigService();
+
+const options = {
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('POSTGRES_USER'),
+    password: configService.get('POSTGRES_PASSWORD'),
+    database: configService.get('POSTGRES_DB'),
+    entities: ['dist/src/data-retriever/entities/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
+    synchronize: true,
+    migrationsRun: true,
+    logging: false,
+    namingStrategy: new SnakeNamingStrategy(),
+    seeds: [InitSeeder],
+};
 
 export const DataSourceConfig: DataSourceOptions = {
     type: 'postgres',
@@ -24,4 +41,5 @@ export const DataSourceConfig: DataSourceOptions = {
     namingStrategy: new SnakeNamingStrategy(),
 };
 
-export const AppDS = new DataSource(DataSourceConfig);
+
+export const AppDS = new DataSource(options as DataSourceOptions & SeederOptions);
