4d92a0694a16dd3eb11528262fe1a9e0046ff4d1
FiboDev <juanpadilla@uninorte.edu.co>
2023-10-15 01:15:18 +0000

    fixed bugs

--- src/data-retriever/data-retriever.service.ts
+++ src/data-retriever/data-retriever.service.ts
@@ -1,86 +1,184 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AudioFeatures } from './entities/audio/audio_features.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import * as fs from 'fs';
 import { parse } from 'csv-parse';
+import { Logger } from '@nestjs/common';
+import { SpotifyArtistRelease } from './entities/spotify/sp_artist_release.entity';
+import { SpotifyReleaseEntity } from './entities/spotify/sp_release.entity';
 
 @Injectable()
 export class DataRetrieverService {
 
-    constructor(@InjectRepository(AudioFeatures) private readonly audioRepository: Repository<AudioFeatures>) {}
+    constructor(@InjectRepository(AudioFeatures) private readonly audioRepository: Repository<AudioFeatures>,
+                @InjectRepository(SpotifyArtistRelease) private readonly spotifyArtistReleaseRepository: Repository<SpotifyArtistRelease>,
+                @InjectRepository(SpotifyReleaseEntity) private readonly spotifyReleaseRepository: Repository<SpotifyReleaseEntity>,
+                                                        private readonly logger: Logger) {}
 
     async retrieveData() {
-        const data = await this.audioRepository.find({ select: {acousticness: true} })
-
-        return data
+        //const data = await this.audioRepository.find({ select: {acousticness: true} })
+        return this.spotifyArtistReleaseRepository.find({ select: {artistId: true, releaseId: true} })
+        //return data
     }
+
+    async readCSVFile(filePath: string): Promise<any[]> {
+
+        return new Promise<any[]>((resolve, reject) => {
+
+          const csvData: any[] = [];
+
+          this.logger.log('Reading CSV file...');
+
+            fs.createReadStream(filePath)
+            .pipe(parse({ delimiter: ',', columns: true }))
+            .on('data', (row) => {
+
+              csvData.push(row);
+
+            })
+            .on('end', () => {
+
+              this.logger.log('CSV file successfully processed.');
+              resolve(csvData);
+
+            })
+            .on('error', (error) => {
+
+              reject(error);
+
+            });
+        });
+      }
 
     async seedAudioFeatures() {
 
         const csvFilePath = 'csv_files/audio_features.csv'
-        const csvData = []
+        const csvData = await this.readCSVFile(csvFilePath)
 
-        console.log(csvFilePath)
+        const totalSize = 2000000
+        const batchSize = 500000
 
-        fs.createReadStream(csvFilePath)
-        .pipe(parse({ delimiter: ',', columns: true }))
-        .on('data', (row) => {
+        const totalLoops = Math.ceil(totalSize / batchSize)
 
-            csvData.push(row);
-        })
-        .on('end', () => {
+        let i = 0
+        let contador = 0
+        let j = 0
 
-            console.log('CSV file successfully processed');
+        this.logger.log('Seeding audio features...');
 
-            console.log(csvData.length)
+        while (i < totalLoops) {
 
-            const totalSize = 2343000
-            const batchSize = 500000
+            console.log("loop " + i)
 
-            const totalLoops = Math.ceil(totalSize / batchSize)
+            while (j < batchSize) {
 
-            let i = 0
-            let contador = 0
-            let j = 0
+                const data = csvData.pop()
 
-            while (i < totalLoops - 1) {
+                if (data === undefined) break
 
-                console.log("loop " + i)
+                console.log(contador)
+                contador += 1
 
-                while (j < batchSize) {
+                this.audioRepository.save(data)
 
-                    const data = csvData.pop()
-
-                    if (data === undefined) break
-
-                    console.log(contador)
-                    contador += 1
-
-                    const audioFeatures = new AudioFeatures()
-
-                    audioFeatures.isrc = data.isrc
-                    audioFeatures.acousticness = data.acousticness
-                    audioFeatures.danceability = data.danceability
-                    audioFeatures.duration_ms = data.duration_ms
-                    audioFeatures.energy = data.energy
-                    audioFeatures.key = data.key
-                    audioFeatures.liveness = data.liveness
-                    audioFeatures.loudness = data.loudness
-                    audioFeatures.mode = data.mode
-
-                    this.audioRepository.save(audioFeatures)
-
-                    j++
-
-                }
-
-                i += 1
-                j = 0
+                j++
 
             }
 
-            console.log("terminado")
-        });
+            i += 1
+            j = 0
+
+        }
+
+        this.logger.log('Audio features successfully seeded.');
+    }
+
+
+    async seedArtistRelease() {
+
+        const csvFilePath = 'csv_files/sp_artist_release.csv'
+        const csvData = await this.readCSVFile(csvFilePath)
+
+        const totalSize = 2000000
+        const batchSize = 500000
+
+        const totalLoops = Math.ceil(totalSize / batchSize)
+
+        let i = 0
+        let contador = 0
+        let j = 0
+
+        this.logger.log('Seeding Artist releases...');
+
+        while (i < totalLoops) {
+
+            console.log("loop " + i)
+
+            while (j < batchSize) {
+
+                const data = csvData.pop()
+
+                if (data === undefined) break
+
+                console.log(contador)
+                contador += 1
+
+                this.spotifyArtistReleaseRepository.save(data)
+
+                j++
+
+            }
+
+            i += 1
+            j = 0
+
+        }
+
+        this.logger.log('Artist releases successfully seeded.');
+
+    }
+
+    async seedRelease() {
+
+        const csvFilePath = 'csv_files/sp_release.csv'
+        const csvData = await this.readCSVFile(csvFilePath)
+
+        const totalSize = csvData.length
+        const batchSize = 500000
+
+        const totalLoops = Math.ceil(totalSize / batchSize)
+
+        let i = 0
+        let contador = 0
+        let j = 0
+
+        this.logger.log('Seeding releases...');
+
+        while (i < totalLoops) {
+
+            console.log("loop " + i)
+
+            while (j < batchSize) {
+
+                const data = csvData.pop()
+
+                if (data === undefined) break
+
+                console.log(contador)
+                contador += 1
+
+                this.spotifyReleaseRepository.save(data)
+
+                j++
+
+            }
+
+            i += 1
+            j = 0
+
+        }
+
+        this.logger.log('Releases successfully seeded.');
     }
 }
